4ad27de2b5b5548f5c4d9ac98acf118d7032aad7
mnabil <supermnabil@gmail.com>
2017-07-01 02:04:17 +0200

    adding csv support

--- goospider/crawl.py
+++ goospider/crawl.py
@@ -3,48 +3,71 @@ import os
 from scrapy import Selector
 from pyvirtualdisplay import Display
 import csv
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 display = Display(visible=0, size=(800, 600))
 display.start()
 
+
+def clean_item(item):
+    for k, v in item.items():
+        if v is None:
+            item[k] = u''
+
+
 driver = webdriver.Firefox()
 urls = ['https://www.google.com/search?q=23100107721',
-        # 'https://www.google.com/search?q=41693392140',
-        # 'https://www.google.com/search?q=49694260437',
-        # 'https://www.google.com/search?q=641265232685',
-        # 'https://www.google.com/search?q=797801035316',
-        # 'https://www.google.com/search?q=14633149517',
-        # 'https://www.google.com/search?q=20626716192',
-        # 'https://www.google.com/search?q=45557180034',
-        # 'https://www.google.com/search?q=43917231709'
+        'https://www.google.com/search?q=41693392140',
+        'https://www.google.com/search?q=49694260437',
+        'https://www.google.com/search?q=641265232685',
+        'https://www.google.com/search?q=797801035316',
+        'https://www.google.com/search?q=14633149517',
+        'https://www.google.com/search?q=20626716192',
+        'https://www.google.com/search?q=45557180034',
+        'https://www.google.com/search?q=43917231709'
         ]
-for url in urls:
-    driver.get(url)
-    source = driver.page_source
-    sel = Selector(text=source)
-    rows = sel.css('._Dw')
-    item = {}
-    item['url'] = unicode(url)
-    for idx,select in enumerate(rows):
-        item['store'+str(idx+1)] = select.css('a > .rhsg4').xpath('text()').extract_first()
-        item['price'+str(idx+1)] = select.css('._kh').xpath('text()').extract_first()
-        item['extras'+str(idx+1)] = select.css('._ree .rhsg4').xpath('text()').extract_first()
-    with open('my_data.csv', 'r+b') as ofile:
-        header = item.keys()
-        dict_writer = csv.DictWriter(ofile, header, -999, extrasaction="ignore")
-        dict_writer.writerow(item)
-        # element = driver.find_element_by_xpath('//*[@id="resultStats"]/nobr')
-    # print element.text
+
+csv_headers = ['ID','Cost','Url', 'Store1', 'Price1', 'Extras1', 'Store2', 'Price2', 'Extras2', 'Store3', 'Price3', 'Extras3',
+               'Store4', 'Price4', 'Extras4', 'Store5', 'Price5', 'Extras5', 'Store6', 'Price6', 'Extras6', 'Store7',
+               'Price7', 'Extras7', 'Store8', 'Price8', 'Extras8', 'Store9', 'Price9', 'Extras9', 'Store10', 'Price10',
+               'Extras10']
+with open('data.csv', 'w') as f:
+    f.writelines('\t'.join(csv_headers) + '\n')
+
+    for url in urls:
+        driver.get(url)
+        print('Scraping URL: %s'%url)
+        source = driver.page_source
+        sel = Selector(text=source)
+        rows = sel.css('._Dw')
+        item = {}
+        item['Url'] = unicode(url)
+
+        for idx, select in enumerate(rows):
+            item['Store' + str(idx + 1)] = select.css('a > .rhsg4').xpath('text()').extract_first()
+            item['Price' + str(idx + 1)] = select.css('._kh').xpath('text()').extract_first().strip(u'$')
+            item['Extras' + str(idx + 1)] = select.css('._ree .rhsg4').xpath('text()').extract_first()
+            item['ID'] = u''
+            item['Cost'] = u''
+
+        clean_item(item)
+        print('Saving data into file: %s'%f.name)
+        f.writelines(u'\t'.join([item[key] for key in csv_headers if key in item.keys()]) + '\n')
+
+f.close()
+
+# element = driver.find_element_by_xpath('//*[@id="resultStats"]/nobr')
+# print element.text
 driver.close()
 
 
-    # def process_item(self, item, spider):
-    #     self.file.writelines(["%s\n" % l for l in item['sellerurls']])
-    #     data_fields = [i for i in item.iterkeys() if i != 'url' and i != 'condition' and i !='sellerurls']
-    #     data = {}
-    #     data['url'] = item['url']
-    #     for field in data_fields:
-    #         for idx, value in enumerate(item[field]):
-    #             data[field + str(idx + 1)] = value
-    #     return data
+# def process_item(self, item, spider):
+#     self.file.writelines(["%s\n" % l for l in item['sellerurls']])
+#     data_fields = [i for i in item.iterkeys() if i != 'url' and i != 'condition' and i !='sellerurls']
+#     data = {}
+#     data['url'] = item['url']
+#     for field in data_fields:
+#         for idx, value in enumerate(item[field]):
+#             data[field + str(idx + 1)] = value
+#     return data
