00ada362994c22aeb2225fd388135856f8277721
mnabil <supermnabil@gmail.com>
2017-07-02 19:29:54 +0200

    fixing typo in spidy

--- selespider/pipelines.py
+++ selespider/pipelines.py
@@ -6,17 +6,15 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 # from items import ScrapemanItem
 # import datetime
+def clean_item(item):
+    for k, v in item.items():
+        if v is None:
+            item[k] = u''
 
 class ScrapemanPipeline(object):
-    def clean_item(item):
-        for k, v in item.items():
-            if v is None:
-                item[k] = u''
-
     def close_spider(self, spider):
-
         self.driver.close()
 
     def process_item(self, item, spider):
-        self.clean_item(item)
+        clean_item(item)
         return item
